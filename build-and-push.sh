2916ef10ff81721b41089c198bcc68fb8056a7c4
Jason Raimondi <jason@raimondi.us>
2018-04-02 23:38:57 -0700

    Update to 1.13.10 and adding build and push script.

---
+++ build-and-push.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+
+docker build -t digitalcanvasdesign/nginx-ssl:$1-alpine alpine/
+docker push digitalcanvasdesign/nginx-ssl:$1-alpine
+docker build -t digitalcanvasdesign/nginx-ssl:$1 debian/
+docker push digitalcanvasdesign/nginx-ssl:$1
